15d511c3fb0f1c1e2a34c59092501fca1dd62d87
Omkar Kudale <omkarkudale007@gmail.com>
2022-11-09 12:46:22 +0530

    added news cards and components

---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import { getByTitle } from '@testing-library/react'
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    render() {
+        let {title, description, imageUrl, nwewsUrl} = this.props
+         return (
+        
+            <div className='my-3'>
+                <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="" className="btn btn-sm btn-primary">Read more</a>
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
